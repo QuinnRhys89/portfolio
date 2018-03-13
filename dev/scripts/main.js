539c10025545cc45c9416a66e3bbe9e6dda86b3c
Quinn Anderson <quinnrhys89@gmail.com>
2018-03-13 10:46:58 -0400

    changed site to a vertical layout

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,5 +1,18 @@
 $('.main-carousel').flickity({
     // options
     cellAlign: 'left',
-    contain: true
+    contain: true,
+    draggable: true,
+    wrapAround: true
+});
+
+//  Scroll Reveal Animation 
+
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.reveal', { duration: 1200 });
+
+// Smooth Scroll
+
+$('a').smoothScroll({
+    offset: 0
 });
